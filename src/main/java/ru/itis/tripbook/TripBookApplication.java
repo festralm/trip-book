601e2f9c80a16ec7e1b74e3bbaf6ba3abbd07f16
Alia Minnegaraeva <alia01minn@gmail.com>
2021-05-07 13:18:29 +0300

    empty project created

---
+++ src/main/java/ru/itis/tripbook/TripBookApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.tripbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/itis/tripbook/TripBookApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itis.tripbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
